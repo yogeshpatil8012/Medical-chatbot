9a315ad1c6578935d28afebd38377838f91d431f
yogeshpatil8012 <ypp928706@tatatechnologies.com>
2026-06-23 23:47:48 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "medical_chatbot",
+    version= "0.1.0",
+    author= "Yogesh Patil",
+    author_email= "yogeshptl62@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+    
+)
